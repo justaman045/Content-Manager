55d40ea13d515545f6a02391f36bd83e5c7a032b
coderaman07 <coderaman07@gmail.com>
2022-08-04 16:20:10 +0530

    Updated the Script with Upgrade to this Script of how Data gets handeled in this

---
+++ Nessesary_Components/Promote.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from Nessesary_Components.rss_json import get_posts_details
+from Manager.GraphicalComponents.OptionBox import PlatformsToUpload
+import pyperclip as clip
+import pymsgbox as pg
+import tweepy
+import praw
+
+
+def Promote(platform):
+    data = get_posts_details(os.getenv(str(platform)))
+    Headline, Links, LinksToUpload = [], [], []
+    for i in data['posts']:
+        Links.append(i['link'])
+        Headline.append(i['title'])
+    PlatformsToUpload(
+        platforms=Headline, title='Select any One to Promote over all your Socials Connected')
+    selectedOptions = str(clip.paste()).split("+")[:-1]
+    htmlLinks, htmlTitles = '', ""
+    for i in range(0, len(selectedOptions)):
+        LinksToUpload.append(Links[Headline.index(selectedOptions[i])])
+        htmlTitles += f"{i+1}. {selectedOptions[i]}\n"
+        htmlLinks += f"{i+1}. {LinksToUpload[i]}\n"
+    PlatformsToUpload(platforms=["Twitter", "Reddit"],
+                      title='Select Your Socials to Upload to')
+    platforms = str(clip.paste()).split("+")[:-1]
+    tempMessage = f"Hii, Just so you get to know\n\nI wrote these Blog Posts Named\n{htmlTitles}\n\n You can access the same from the links below \n{htmlLinks}"
+    if len(tempMessage) > 280:
+        pg.alert(
+            f"Your Promotion Message Exceds the Message Length Reduce the Number of Links to Promote.\n\nThe length of this Message would be {len(tempMessage)}\n\nThis Program will now Quit Please Try again", os.getenv("BotName"))
+        sys.exit()
+    if platform in ["YouTube", "Twitch"]:
+        if len(LinksToUpload) == 1:
+            message = f"Hii, Just so you get to know\n\nI made this Video Named\n\n{htmlTitles}\n\n You can access the same from the links below \n\n{htmlLinks[3:]}"
+        else:
+            message = f"Hii, Just so you get to know\n\nI made these Videos Named\n\n{htmlTitles}\n\n You can access the same from the links below \n\n{htmlLinks}"
+    else:
+        if len(LinksToUpload) == 1:
+            message = f"Hii, Just so you get to know\n\nI wrote this Blog Post Named\n\n{htmlTitles}\n\n You can access the same from the links below \n\n{htmlLinks[3:]}"
+        else:
+            message = f"Hii, Just so you get to know\n\nI wrote these Blog Posts Named\n\n{htmlTitles}\n\n You can access the same from the links below \n\n{htmlLinks}"
+    for i in platforms:
+        if i == "Twitter":
+            auth = tweepy.OAuthHandler(
+                os.getenv("ConsumerKeyTwitter"), os.getenv("ConsumerKeySecretTwitter"))
+            auth.set_access_token(
+                os.getenv("AccesTokenTwitter"), os.getenv("AccessTokenSecretTwitter"))
+
+            api = tweepy.API(auth)
+            client = tweepy.Client(consumer_key=os.getenv("ConsumerKeyTwitter"), consumer_secret=os.getenv("ConsumerKeySecretTwitter"),
+                                   access_token=os.getenv("AccesTokenTwitter"), access_token_secret=os.getenv("AccessTokenSecretTwitter"), bearer_token=os.getenv("BearerTokenTwitter"))
+            client.create_tweet(text=message)
+        if i == "Reddit":
+            pass
--- main.py
+++ main.py
@@ -1,41 +1,54 @@
+# Import all the nessesary Modules 
 import os
+import sys
 import pymsgbox as pg
-from Manager.GraphicalComponents.OptionBox import PlatformsToUpload
 from Manager.ProjektNotify.ProjektNotify import ProjektNotify
 from Manager.CodeItDown.codeitDown import CodeItDown
 from Manager.AutoBlogPost.main import AutoBlogPost
+from Nessesary_Components.Promote import Promote
 from Nessesary_Components.ScriptUpdate import GetUpdate
-from Nessesary_Components.rss_json import get_posts_details
-import pyperclip as clip
 from dotenv import load_dotenv
 
+# Load the Environment Variables written in .env file 
 load_dotenv()
 
-try:
-    GetUpdate()
-except:
-    pass
+# Check for Update then notify the User 
+# GetUpdate()
 
+# What the user wants to do 
 option = pg.confirm("Select your option to Manage", os.getenv("BotName"), buttons=['Mail', 'Promote Post', "Blog Post"])
 
+# If It's mail then send the mail 
 if option == 'Mail':
+
+    # To whome the user wants to send mail either it is Blog Subscribers or the waiting List Subscribers 
     mailOption = pg.confirm('Whome to Mail??', os.getenv("BotName"), buttons=['Code it Down Subscribers', 'Projekt Notify Subscribers'])
+    
+    # If Blog Subscribers then send mail to them 
     if mailOption == 'Code it Down Subscribers':
         CodeItDown()
+
+    # else if to Product Waiting List Subscribers 
     elif mailOption == 'Projekt Notify Subscribers':
         ProjektNotify()
+
+    # If no one then Quit 
     else:
-        exit()
+        sys.exit()
+
+# If the User Wants to Promote the Blog Post/Videos 
 elif option == 'Promote Post':
+
+    # Choose what to Promote from either it is from Medium, YouTube or what??
     platform = pg.confirm("From which Platform you want to Promote your Post", os.getenv("BotName"), buttons=['CodeitDown', "HashNode", "Dev.to", "Medium", "YouTube", "Twitch"])
-    data = get_posts_details(os.getenv(str(platform)))
-    Headline = []
-    for i in data['posts']:
-        Headline.append(i['title'])
-    PlatformsToUpload(platforms=Headline, title='Select any One to Promote over all your Socials Connected')
-    if len(str(clip.paste())[:-1].split('+')) >= 2:
-        pg.alert("Taking the Latest Headline to Promote as Promotion can be only Occured for 1 Only")
+
+    # Now Promote it 
+    Promote(platform)
+
+# If User wants to Write a Blog Post then Call it's respective function 
 elif option == 'Blog Post':
     AutoBlogPost()
+
+# If Nothing then Quit 
 else:
-    exit()
+    sys.exit()
